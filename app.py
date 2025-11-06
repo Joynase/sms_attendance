bc307a4cbf708d4bc7a357aa19bb42a7746d0489
Joynase <dimejoynase@isufst.edu.ph>
2025-11-06 22:08:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -31,5 +31,9 @@ def index():
         status = f"Response: {response}"
     return render_template("index.html", status=status)
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
